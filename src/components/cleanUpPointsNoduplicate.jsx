2985fbd069e29c60a3bfb900a029cafb92c7f117
CharleneTSJ <charlenetansijie@outlook.com>
2023-11-06 22:32:49 +0800

    new 1

--- src/components/cleanUpPointsNoduplicate.jsx
+++ src/components/cleanUpPointsNoduplicate.jsx
@@ -28,7 +28,7 @@ function replaceObjects(resultObjects, finalArray) {
 	
 	return updatedResultObjects;
       }
-exports = {replaceObjects};
+export {replaceObjects};
       // Example data resultObjects is from flightPlans.jsx
 // const resultObjects = [
 // 	{ KAT: [[13.03, 7.69], [-33.71, 150.3], [7.16, 79.87]] },
